87f39fe2bf3a4fd0f0559442266c659fc80cfbaa
arzummammadova <arzuam-af106@code.edu.az>
2025-07-04 00:27:49 +0400

    register verify email

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ import express from 'express';
 import 'dotenv/config'
 import './src/db/connectionDB.js'
 import router from './src/routers/authRoutes.js';
+import cors from 'cors';
 const app=express();
 const PORT=process.env.PORT || 5000;
+app.use(cors())
 import { swaggerDocs } from './src/utils/swagger.js';
 
 
